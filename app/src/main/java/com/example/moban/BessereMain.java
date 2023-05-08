0c2b54221973341cd0928e4c666f8ae808d440ff
NoahGora <81632511+NoahGora@users.noreply.github.com>
2023-05-09 00:07:50 +0200

    Test Bessere Main mit RecyclerView

---
+++ app/src/main/java/com/example/moban/BessereMain.java
@@ -0,0 +1,14 @@
+package com.example.moban;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BessereMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
