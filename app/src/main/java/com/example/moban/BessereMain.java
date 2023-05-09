c65456149a594bd2c4f3190906549bd5f3acea1a
NoahGora <81632511+NoahGora@users.noreply.github.com>
2023-05-09 12:41:12 +0200

    Test weiter

--- app/src/main/java/com/example/moban/BessereMain.java
+++ app/src/main/java/com/example/moban/BessereMain.java
@@ -1,14 +1,38 @@
 package com.example.moban;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BessereMain extends AppCompatActivity {
 
+    EditText newTaskEditText;
+    ImageView sendButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
     }
+
+        protected void initViews(){
+            newTaskEditText = findViewById(R.id.new_task);
+            sendButton = findViewById(R.id.send_button);
+
+
+        }
+
+        protected void initClickListener(){
+            sendButton.setOnClickListener(view -> onSendButtonClick());
+        }
+
+        protected void onSendButtonClick(){
+            String newTaskName = newTaskEditText.getText().toString();
+            Log.d("Es funktioniert", "Neue Aufgabe: " + newTaskName);
+        }
 }
